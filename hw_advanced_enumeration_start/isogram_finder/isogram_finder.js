a158b503b525079a94be6d4fe48e38e2ff48bee8
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-09 20:53:15 +0000

    isogram coded and tested

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,8 +1,28 @@
 const IsogramFinder = function (word) {
 
+    this.word = word.split('');
+
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+
+    uniqueLetters = [];
+
+    const lowerCased = this.word.map((letter) => {
+        return letter.toLowerCase();   
+    });
+
+    const result = lowerCased.every((letter) => {
+        if (uniqueLetters.indexOf(letter) === -1) {
+            uniqueLetters.push(letter);
+            return true;
+        } else {
+            return false;
+        }
+
+    });
+  
+    return result;
 
 }
 
